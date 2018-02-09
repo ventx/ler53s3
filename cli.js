369969a7bd84bb111984a07dc30a63a9ad230dc8
Wolfgang Felbermeier <wolfgang@ventx.de>
2018-02-09 15:28:48 +0100

    fix cli

--- cli.js
+++ cli.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
 const cli = require('cli');
 
 let options = cli.parse({
 	domain: ['d', 'The domain to create a certificate for', 'string'],
 	email: ['e', 'The email for the certification request', 'string'],
-	zone: ['z', 'The name of the managed route53 zone, the domain is part of', 'string'],
+	zoneName: ['z', 'The name of the managed route53 zone, the domain is part of', 'string'],
 	bucketName: ['b', 'The bucket to store the certificate to', 'string'],
 	keyPrefix: ['k', 'The key prefix to store the certificate to', 'string']
 });
